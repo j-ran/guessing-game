45f6757589ab7e72962bfd5e2127a3c5784b50fa
Janaki Ranpura <automatic.janaki@gmail.com>
2021-01-06 23:23:04 +0000

    unedited guessing game

---
+++ game.py
@@ -0,0 +1,31 @@
+"""A number-guessing game."""
+# Put your code here
+
+import random
+
+def guessing_game():
+#greet player
+    print("Hello, welcome to the game")
+#get player name
+
+    name = input("Hi, What is your name? > ") 
+    print('Welcome,', name, ' give me a random number between 1 and 100')
+    # username = input("Enter username: ")
+    # print(username)
+
+
+
+
+#choose random number between 1 and 100
+#repeat forever:
+
+#     get guess
+#     if guess is incorrect:
+#         give hint
+#         increase number of guesses
+#     else:
+#         congratulate player
+
+
+guessing_game()
+
